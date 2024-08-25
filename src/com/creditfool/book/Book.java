bb9e8060ed9b1922f0512662984594814c60851e
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-08-25 17:33:29 +0700

    add csv output into gitignore

--- src/com/creditfool/book/Book.java
+++ src/com/creditfool/book/Book.java
@@ -1,5 +1,6 @@
 package com.creditfool.book;
 
+import java.util.List;
 import java.util.Objects;
 
 public abstract class Book {
@@ -7,61 +8,32 @@ public abstract class Book {
     protected String title;
     private Integer releaseYear;
 
-    public Book() {
-        this.id = createId();
-    }
-
-    public Book(String title, Integer releaseYear) {
-        this.title = title;
-        this.releaseYear = releaseYear;
-        this.id = createId();
-    }
-
     public Book(String id, String title, Integer releaseYear) {
-        this.title = title;
-        this.releaseYear = releaseYear;
         this.id = id;
-    }
-
-    abstract String createId();
-
-    public abstract String getTitle();
-
-    public String getId() {
-        return id;
-    }
-
-    public void setTitle(String title) {
         this.title = title;
-    }
-
-    public Integer getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(Integer releaseYear) {
         this.releaseYear = releaseYear;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return Objects.equals(id, book.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
     }
 
     @Override
     public String toString() {
-        return "Book{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", releaseYear=" + releaseYear +
-                '}';
+        return "Book [id=" + id + ", title=" + title + ", releaseYear=" + releaseYear + "]";
     }
+
+    public String getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    
 }
